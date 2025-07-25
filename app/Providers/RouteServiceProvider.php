03a720c5ec9421ff4d0f1fba01b3dc0e016a057c
ducnmald <ducnm@aladdin-jsc.com>
2025-07-25 11:06:51 +0700

    udpate base

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,10 +31,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
-                ->group(__DIR__ . '/../routers/api.php');
+                ->group(__DIR__ . '/../../routers/api.php');
 
             Route::middleware('web')
-                ->group(__DIR__ . '/../routers/web.php');
+                ->group(__DIR__ . '/../../routers/web.php');
         });
     }
 }
